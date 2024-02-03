5e975c12396b8bc95ee22f2a4b222af129ad78f0
Amisha PRIYA <priyaamisha03@gmail.com>
2024-02-04 02:05:34 +0530

    Setting up tailwind CSS in project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # styling-react-app-using-tailwind-css
 
 Styling a react application using Tailwind CSS framework
+
+Setting up tailwind CSS in a Vite project : https://tailwindcss.com/docs/guides/vite
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Add the paths to all of your template files
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
