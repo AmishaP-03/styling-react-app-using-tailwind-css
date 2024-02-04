4e06c50cf4c0d8394d959222a244f37ce4d9d799
Amisha PRIYA <priyaamisha03@gmail.com>
2024-02-04 15:25:08 +0530

    Handling media queries using tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <header className='flex flex-col items-center mt-8 mb-16'> {/* Tailwind utility classes added here. mt ---> magin-top and so on. */}
+    <header className='flex flex-col items-center mt-8 mb-8 md:mb-16'> {/* Tailwind utility classes added here. mt ---> magin-top and so on. */}
       <img src={logo} alt="A canvas" className='mb-8 w-44 h-44 object-contain'/> {/* object-contain ---> makes sure that the object is not distorted */}
-      <h1 className='text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-customFontFromGoogle'>ReactArt</h1>
+      <h1 className='text-xl md:text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-customFontFromGoogle'>ReactArt</h1>
       {/* tracking-widest ---> Add spacing between the characters
       text-amber-800 ---> adds text color */}
       <p className='text-stone-500'>A community of artists and art-lovers.</p>
