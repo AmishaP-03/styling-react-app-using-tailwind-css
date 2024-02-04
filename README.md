058463ce1ac3b91eda491a56fd3c2c9885d20803
Amisha PRIYA <priyaamisha03@gmail.com>
2024-02-04 14:56:00 +0530

    Adding utility classes to header and its child elements

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # styling-react-app-using-tailwind-css
 
-Styling a react application using Tailwind CSS framework
+Styling a react application using Tailwind CSS framework. Add tiny utility classes (which have predefined CSS) to our elements to change their looks.
 
 Setting up tailwind CSS in a Vite project : https://tailwindcss.com/docs/guides/vite
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import logo from '../assets/logo.png';
 
 export default function Header() {
   return (
-    <header>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+    <header className='flex flex-col items-center mt-8 mb-16'> {/* Tailwind utility classes added here. mt ---> magin-top and so on. */}
+      <img src={logo} alt="A canvas" className='mb-8 w-44 h-44 object-contain'/> {/* object-contain ---> makes sure that the object is not distorted */}
+      <h1 className='text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 '>ReactArt</h1>
+      {/* tracking-widest ---> Add spacing between the characters
+      text-amber-800 ---> adds text color */}
       <p>A community of artists and art-lovers.</p>
     </header>
   );
